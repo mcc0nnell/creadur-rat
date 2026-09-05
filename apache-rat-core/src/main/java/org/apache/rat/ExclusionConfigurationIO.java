bf79a1bde7fb1cd369ba70e6cc39c65986e19c6f
Robert McConnell <robert@mcc0nnell.org>
2026-09-05 12:16:02 -0400

    RAT-554: satisfy JavadocVariable checkstyle

--- apache-rat-core/src/main/java/org/apache/rat/ExclusionConfigurationIO.java
+++ apache-rat-core/src/main/java/org/apache/rat/ExclusionConfigurationIO.java
@@ -2,9 +2,8 @@
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
+ * regarding copyright ownership.  The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
@@ -41,6 +40,7 @@ import org.xml.sax.SAXException;
  */
 public final class ExclusionConfigurationIO {
 
+    /** Root element used by the exclusion configuration serialization. */
     private static final String ROOT = "ExclusionProcessor";
 
     private ExclusionConfigurationIO() {
